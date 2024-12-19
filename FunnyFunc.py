536d62423db4a4c32d792b1ac2c4204c19feab3b
Xaabibas <tumuyhabirov@gmail.com>
2024-12-19 18:05:03 +0300

    Add a funny funciton

---
+++ FunnyFunc.py
@@ -0,0 +1,9 @@
+def FunnyFunc():
+	print("                 0   ")
+	print("                /|\\$ ")
+	print("                /^\\  ")
+	print("This is the man ^^^")
+	print("                    /0\\  ")
+	print("                    /|\\  ")
+	print("                    / \\  ")
+	print("And this is a woman ^^^")
---
+++ dop.py
@@ -0,0 +1,4 @@
+from FunnyFunc import FunnyFunc as ff
+
+
+ff()
